5fba04fba768c5f6c78766a26698096f9b7980ed
Yessi Raya <ysnraya@gmail.com>
2020-11-05 11:12:44 -0700

    modal popup and code

--- sw-starships/main.js
+++ sw-starships/main.js
@@ -5,6 +5,19 @@ const nav = document.querySelector('.nav')
 const navList = document.querySelector('.navList')
 const shipView = document.querySelector('.main')
 
+const dialog = document.querySelector('.modal')
+const closeButton = document.querySelector('.modal-close')
+const modalBackground = document.querySelector('.modal-background')
+
+
+closeButton.addEventListener('click', () => {
+    dialog.classList.toggle("is-active")
+})
+
+modalBackground.addEventListener('click', () => {
+    dialog.classList.toggle("is-active")
+})
+
 function populateNav(starships) {
     starships.forEach(starship => {
         let anchorWrap = document.createElement('a')
@@ -33,7 +46,10 @@ function populateShipView(shipData) {
     //set their source to the url below
     shipImage.src = `https://starwars-visualguide.com/assets/img/starships/${shipNum}.jpg`
     //make Shipnum act like charnum from the characters page
-    shipImage.addEventListener('error', () => shipImage.hidden = true)
+    shipImage.addEventListener('error', () => {
+    shipImage.hidden = true
+    dialog.classList.toggle("is-active")
+    })
     //append the image to the shipView element
     shipView.appendChild(shipImage)
 }
