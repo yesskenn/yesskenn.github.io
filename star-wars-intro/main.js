57aecbd640236c1abae77bf3e369153c56b96fd4
Yessi Raya <ysnraya@gmail.com>
2020-11-10 16:25:21 -0700

    added a new intro page with links, more cohesive stylization

---
+++ star-wars-intro/main.js
@@ -0,0 +1,12 @@
+const starshipsButton = document.createElement('button')
+starshipsButton.textContent = 'Starships'
+mainHeader.appendChild(starshipsButton)
+
+
+const charButton = document.createElement('button')
+charButton.textContent = 'Characters'
+mainHeader.appendChild(charButton)
+
+const filmsButton = document.createElement('button')
+filmsButton.textContent = 'Films'
+mainHeader.appendChild(filmsButton)
