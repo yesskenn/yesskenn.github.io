126ed75630dfe12db2b389172ca1b75f0f230a19
Yessi Raya <ysnraya@gmail.com>
2020-11-10 18:30:59 -0700

    added star wars intro page with links

--- star-wars-intro/main.js
+++ star-wars-intro/main.js
@@ -1,12 +1,19 @@
+
 const starshipsButton = document.createElement('button')
-starshipsButton.textContent = 'Starships'
+// starshipsButton.textContent = 'Starships'
+starshipsButton.innerHTML = '<a href="../sw-starships">STARSHIPS</a>'
 mainHeader.appendChild(starshipsButton)
 
 
 const charButton = document.createElement('button')
-charButton.textContent = 'Characters'
+// charButton.textContent = 'Characters'
+charButton.innerHTML = '<a href="../star-wars-characters">CHARACTERS</a>'
 mainHeader.appendChild(charButton)
 
 const filmsButton = document.createElement('button')
-filmsButton.textContent = 'Films'
+// filmsButton.textContent = 'Films'
+filmsButton.innerHTML = '<a href="../star-wars-films">FILMS</a>'
 mainHeader.appendChild(filmsButton)
+
+
+
