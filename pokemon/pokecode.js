0b693ef0d5c914be8c624f30b6bcb6b9c7acb870
Yessi Raya <ysnraya@gmail.com>
2020-11-17 11:38:01 -0700

    added api calls, need to add images

--- pokemon/pokecode.js
+++ pokemon/pokecode.js
@@ -1,3 +1,5 @@
+
+
 //Reusable async function to fetch data from URL
 
 async function getAPIData(url) {
@@ -26,14 +28,37 @@ function loadPage() {
 
 const pokemonGrid = document.querySelector('.pokeGrid')
 
-
 function populatePokeCard(pokemon) {
+    let pokeScene = document.createElement('div')
+    pokeScene.className = 'scene'
+    let pokeCard = documment.createElement('div')
+    pokeCard.className = 'card'
     let cardFront = document.createElement('div')
     let frontLabel = document.createElement('p')
+    let frontImage = document.createElement('img')
+    let cardBack = document.createElement('div')
+    let backLabel = document.createElement('p')
+
 
     frontLabel.textContent = pokemon.name
+    frontImage.src = `../image/pokemon/${getImageFileName(pokemon)}.png`
+    backLabel.textContent = `I'm the back of the card`
+    cardFront.appendChild(frontImage)
     cardFront.appendChild(frontLabel)
-    pokemonGrid.appendChild(cardFront)
+    cardBack.appendChild(backLabel)
+    pokeCard.appendChild(cardFront)
+    pokeCard.appendChild(cardBack)
+    pokeScene.appendChild(pokeCard)
+    pokeGrid.appendChild(pokeScene)
 }
 
-loadPage()
+function getImageFileName(pokemon) {
+    if (pokemon.id < 10) {
+        return `--${pokemon.id}`
+    } else if (pokemon.id >9 && pokemon.id <99) {
+        return `0${pokemon.id}`
+    }
+}
+
+
+//loadPage()
