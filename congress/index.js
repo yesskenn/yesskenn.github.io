6159af0265e338ffa7c8f0c59321e32530e5ff8c
Yessi Raya <ysnraya@gmail.com>
2020-12-04 09:57:12 -0700

    added congress page

---
+++ congress/index.js
@@ -0,0 +1,82 @@
+import { senators } from '../data/senators.js'
+import { removeChildren } from '../utils/index.js'
+
+const senatorGrid = document.querySelector('.senatorGrid')
+const seniorityButton = document.querySelector('#seniorityButton')
+const birthdayButton = document.querySelector('#birthdayButton')
+
+
+seniorityButton.addEventListener('click', () => {
+    seniortySort()
+})
+
+birthdayButton.addEventListener('click', () => {
+    birthdaySort()
+})
+
+function populateSenatorDiv(simpleSenators) {
+    removeChildren(senatorGrid)
+    simpleSenators.forEach(senator => {
+        let senDiv = document.createElement('div')
+        let senFigure = document.createElement('figure')
+        let figImg = document.createElement('img')
+        let figCaption = document.createElement('figcaption')
+        let partyIcon = document.createElement('i')
+        if (senator.party === 'R') partyIcon.className = 'fas fa-republican'
+        if (senator.party === 'D') partyIcon.className = 'fas fa-democrat'
+        if (senator.party === 'ID') partyIcon.className = 'fas fa-star'
+        figImg.src = senator.imgURL
+        figCaption.textContent = senator.name
+
+        figCaption.appendChild(partyIcon)
+        senFigure.appendChild(figImg)
+        senFigure.appendChild(figCaption)
+        senDiv.appendChild(senFigure)
+        //senDiv.appendChild(progressBars(senator))
+        senatorGrid.appendChild(senDiv)
+    })
+}
+
+function getSiomplifiedSenators(senatorArray) {
+    return senatorArray.map(senator => {
+        let middleName = senator.middle_name ? ` ${senator.middle_name}` : ` `
+        return {
+            id: senator.id,
+            name: `${senator.first_name}${middleName}${senator.last_name}`,
+            imgURL: `https://www.govtrack.us/static/legislator-photos/${senator.govtrack_id}-200px.jpeg`,
+            seniority: parseInt(senator.seniority, 10),
+            missedVotesPct: senator.missed_votes_pct,
+            party: senator.party,
+            loyaltyPct: senator.votes_with_party_pct,
+            date_of_birth: senator.date_of_birth
+        }
+    })
+}
+
+const filterSenators = (prop, value) => {
+    return getSimplifiedSenators(senators).filter(senator => {
+        return senator[prop] === value
+    })
+}
+const republicans = filterSenators('party', 'R')
+const democrats = filterSenators('party', 'D')
+
+const mostSeniority = getSimplifiedSenators(senators).reduce((acc, senator) => acc.seniority ? acc : senator)
+
+const missedVotes = getSimplifiedSenators(senators).reduce((acc, senator) => acc.missedVotesPct > senator.missedVotesPct ? acc: senator)
+
+function birthdaySort() {
+    populateSenatorDiv(getSimplifiedSenators(senators).sort((a,b) => {
+        return a.date_of_birth - b.date_of_birth
+    }))
+}
+
+
+function senioritySort() {
+    populateSenatorDiv(getSimplifiedSenators(senators).sort((a,b) => {
+        return a.seniority - b.seniority
+    }))
+}
+
+console.log(mostSeniority, missedVotes, republicans, democrats)
+populateSenatorDiv(getSimplifiedSenators(senators))
