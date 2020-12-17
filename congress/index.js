b1ebd4b2f6c4922b6dcc726cfddd23603fb4aa1d
Yessi Raya <ysnraya@gmail.com>
2020-12-17 13:03:46 -0700

    functioning cards, need alphabetical sort, and append new card

--- congress/index.js
+++ congress/index.js
@@ -10,10 +10,10 @@ const senateGrid = document.querySelector('.senateGrid')
 const demButton = document.querySelector('#demButton')
 const repButton = document.querySelector('#repButton')
 const indButton = document.querySelector('#indButton')
-//const stateButton = document.querySelector('#stateButton')
+const stateButton = document.querySelector('#stateButton')
 
 
-//EVENT LISTENERS FOR PARTY AND STATE BUTTONS
+//EVENT LISTENERS FOR PARTY AND STATE BUTTONS USING FILTER
 const democrats = senators.filter((senator) => senator.party === 'D')
 demButton.addEventListener('click', () => populateSenateGrid(democrats))
 
@@ -23,25 +23,18 @@ repButton.addEventListener('click', () => populateSenateGrid(republicans))
 const independents = senators.filter((senator) => senator.party === 'ID')
 indButton.addEventListener('click', () => populateSenateGrid(independents))
 
-//stateButton.addEventListener('click', () => populateSenateGrid(stateSort()))
-/*const yourContactInfo = document.querySelector('#yourContactInfo')
-yourContactInfo.addEventListener('click', () => {
-    let yourContactInfo = prompt('What is Your Email?'))
-    let newSenator = new Senator(
+stateButton.addEventListener('click', () => populateSenateGrid(senators.state.sort()))
 
-        id: senator.id,
-        name: `${senator.first_name} ${senator.last_name} `,
-        imgURL: `https://www.govtrack.us/static/legislator-photos/${senator.govtrack_id}-200px.jpeg`,
-        state: senator.state,
-        address: senator.office,
-        party: senator.party,
-        twitter_account: senator.twitter_account,
-        phone: senator.phone,
-        contact_form: senator.contact_form,
+//STATE SORT IN ALPHABETICAL ORDER
+
+/*function (senators.state).sort() {
+    state.sort((a, b) =>  (a.senators.state).localeComapre(b.senators.state))
+};*/
+
+const yourContactInfo = document.querySelector('#yourContactInfo')
+yourContactInfo.addEventListener('click', () => {
+    let yourContactInfo = prompt('What is Your Email?')}
     )
-   
-   populateSenDiv(newSenator)
-    )*/
 
 
 //CREATING THE CARD
@@ -64,31 +57,7 @@ function populateSenateGrid(senatorData) {
 
 })
 }
-//FRONT ELEMENTS
-   /* let senFigure = document.createElement('figure')
-    let figImg = document.createElement('img')
-    let figCaption = document.createElement('figcaption')
-    let partyIcon = document.createElement('i')
-    let senateState = document.createElement('p')
-    if (senator.party === 'R') partyIcon.className = 'fas fa-republican'
-    if (senator.party === 'D') partyIcon.className = 'fas fa-democrat'
-    if (senator.party === 'ID') partyIcon.className = 'fas fa-star'
-   
-    senateState.textContent = `State: ${senator.state}`
-    
-    figImg.src = senator.imgURL
-    figCaption.textContent = senator.name
-    senateState.textContent = `State: ${senator.state}`
-    
-    //CARD FRONT
-    figCaption.appendChild(partyIcon)
-    senFigure.appendChild(figImg)
-    senFigure.appendChild(figCaption)
-    senDiv.appendChild(senFigure)
-    senDiv.appendChild(senateState)
-    senateGrid.appendChild(senDiv)
-    })
-}/*/
+
 
 //MAP OF NEEDED PROPERTIES
 function simpleSenators(senArray) {
@@ -114,7 +83,9 @@ function populateCardFront(senator) {
     cardFront.className = `card-face card-face-front`
     let frontImage = document.createElement('img')
     frontImage.src = senator.imgURL
-    
+    let bigName = document.createElement('h3')
+    cardFront.className = `bigName`
+    bigName.textContent = `${senator.name}`
     
     let senateState = document.createElement('p')
 
@@ -124,10 +95,11 @@ function populateCardFront(senator) {
     if (senator.party === 'ID') partyIcon.className = 'fas fa-star'
    
     senateState.textContent = `State: ${senator.state}`
-
+   
     cardFront.appendChild(frontImage)
-    cardFront.appendChild(partyIcon)
     cardFront.appendChild(senateState)
+    cardFront.appendChild(bigName)
+    bigName.appendChild(partyIcon)
     return cardFront
 }
 
@@ -136,7 +108,7 @@ function populateCardBack(senator) {
     let cardBack = document.createElement('div')
     cardBack.className = `card-face card-face-back`
     let contactLabel = document.createElement('h2')
-    contactLabel.textContent = `Name: ${senator.name}`
+    contactLabel.textContent = ` ${senator.name}`
    
     let addressLabel = document.createElement('p')
     addressLabel.textContent = `Address: ${senator.address}`
@@ -159,13 +131,23 @@ function populateCardBack(senator) {
     return cardBack
 }
 
-//STATE SORT IN ALPHABETICAL ORDER
 
-/*function stateSort() {
-       populateSenateGrid(senators.state.sort((a, b) => (a.state).localeCompare(b.state)))
+
+
+
+
+  // CREATE NEW CONTACT CARD
+
+/* function yourContactInfo(name, phone, email) {
+        this.name = name
+        this.phone = phone 
+        this.email = email
+       
+        }
+
+let newContact = new Senator() */
+
         
-       }*/
-
 
 //ALL SENATORS
 populateSenateGrid(simpleSenators(senators))
