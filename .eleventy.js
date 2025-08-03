4679ed4377a255dc36e99a8b0197753ab64824e1
Yessi Kenney <yessikenney@gmail.com>
2025-08-03 10:12:24 -0700

    text edits for 11ty file

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,9 @@
 module.exports = function(eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("src/css"); 
   return {
     dir: {
-      input: "src",      // your source folder
-      output: "_site"    // where the built site will go
+      input: "src",      // source folder
+      output: "."    // where the built site will go
     }
   };
 };
