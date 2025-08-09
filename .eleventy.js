5db7368ca27f78a8c2472e26c4f8736d75808aa3
Yessi Kenney <yessikenney@gmail.com>
2025-08-09 11:24:55 -0700

    test

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,9 @@
 module.exports = function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("src/css"); 
+  eleventyConfig.addPassthroughCopy("src/css"); 
   return {
     dir: {
-      input: "src",      // source folder
-      output: "docs"    // where the built site will go
+      input: "src",
+      output: "."
     }
   };
 };
