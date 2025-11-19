27f357d8f1466aab2e5a6cc05a43442aa718eb4d
Yessi Kenney <yessikenney@gmail.com>
2025-11-19 09:45:09 -0800

    fixinng files i hope

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,6 @@ import solidJs from "@astrojs/solid-js"
 // https://astro.build/config
 export default defineConfig({
   site: "https://yesskenn.github.io",
+  base: "",
   integrations: [mdx(), sitemap(), solidJs(), tailwind({ applyBaseStyles: false })],
 })
