88cd203680efee8d06c05821e1cdbfda313b6895
Yessi Kenney <yessikenney@gmail.com>
2025-11-18 18:31:55 -0800

    forgot a comma

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import solidJs from "@astrojs/solid-js"
 // https://astro.build/config
 export default defineConfig({
   site: "https://yesskenn.github.io",
-  base: '/yesskenn.github.io'
+  base: '/yesskenn.github.io',
   integrations: [mdx(), sitemap(), solidJs(), tailwind({ applyBaseStyles: false })],
 })
